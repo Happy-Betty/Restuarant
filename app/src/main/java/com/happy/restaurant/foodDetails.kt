1caa14eef32cad9988915ba33fc0a2f3d3b4ab99
Happy-Betty <happybetty1999@gmail.com>
2022-06-28 16:25:01 +0300

    app showing a restaurant menu

---
+++ app/src/main/java/com/happy/restaurant/foodDetails.kt
@@ -0,0 +1,17 @@
+package com.happy.restaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class foodDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_details)
+        val bundle = intent.extras
+        findViewById<ImageView>(R.id.ivFoodImage).setImageResource(bundle!!.getInt("image"))
+        findViewById<TextView>(R.id.tvName) .text = bundle!!.getString("name")
+        findViewById<TextView>(R.id.tvDetails).text = bundle!!.getString("des")
+    }
+}
